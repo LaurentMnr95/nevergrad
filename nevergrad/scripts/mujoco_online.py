739eebe71b5c9e0c28e11ab37525f155bda6a748
Laurent Meunier <laurentmeunier@fb.com>
2020-11-27 12:18:35 +0100

    mujoco_online script draft

--- nevergrad/scripts/mujoco_online.py
+++ nevergrad/scripts/mujoco_online.py
@@ -28,9 +28,13 @@ class GenericMujocoEnv:
         self.num_rollouts = num_rollouts
         self.env.seed(random_state)
 
-    def __call__(self, x, state_mean, state_std):
+    def __call__(self, x, global_stats):
         """Compute average cummulative reward of a given policy.
         """
+        state_mean = global_stats.mean
+        state_std = global_stats.mean
+
+        stats_run = Stats()
         returns = []
         for _ in range(self.num_rollouts):
             obs = self.env.reset()
@@ -39,27 +43,53 @@ class GenericMujocoEnv:
             while not done:
                 action = np.dot(x, (obs - state_mean) / state_std)
                 obs, r, done, _ = self.env.step(action)
+                stats_run.n += 1
+                stats_run.sum_obs += obs
+                stats_run.sum_obs2 += obs ** 2
+
                 totalr += r
             returns.append(totalr)
 
-        return -np.mean(returns), 0, 0  # todo: return stats of runs
+        return -np.mean(returns), stats_run
 
 
-def update_statistics(state_mean, state_std, stats_run):
-    return ...
+class Stats:
+    def __init__(self):
+        self.n = 0
+        self.sum_obs = 0
+        self.sum_obs2 = 0
+
+    def update(self, stats_run):
+        self.n += stats_run.n
+        self.sum_obs += stats_run.sum_obs
+        self.sum_obs2 += stats_run.sum_obs2
+
+    @property
+    def mean(self):
+        if self.n == 0:
+            return 0
+        return self.sum_obs / self.n
+
+    @property
+    def std(self):
+        if self.n == 0:
+            return 1
+        return self.sum_obs2 / self.n - (self.sum_obs / self.n) ** 2
 
 
 num_workers = 5
+budget = ...
 optimizer = ...
 env_name = ...
 num_rollouts = ...
 random_state = ...
-state_mean, state_std = 0, 0
+global_stats = Stats()
 
+elapsed_budget = 0
 while True:
     x = []
     for _ in range(num_workers):
-        x.append((optimizer.ask(), state_mean, state_std))
+        x.append((optimizer.ask(), global_stats))
 
     env = GenericMujocoEnv(env_name, num_rollouts, random_state)
 
@@ -67,8 +97,10 @@ while True:
         y = p.map(env, x)
 
     for _, stats_run in y:
-        state_mean, state_std = update_statistics(state_mean, state_std, stats_run)
+        global_stats.update(stats_run)
 
     for u, v in zip(x, y):
         optimizer.tell(u, v[0])
-
+        elapsed_budget += 1
+        if elapsed_budget > budget:
+            break
